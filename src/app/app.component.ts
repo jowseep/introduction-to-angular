faad13704af037e71cbb9409822f6fc1fb9480cc
Joseph <sts-joseph.callao@rakuten.com>
2023-06-20 15:23:06 +0800

    created housing app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HousingLocation } from './housing-location';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'fairhouse';
+
+  housingLocationList: HousingLocation[] = [
+    {
+      name: "Joseph Fresh Start Housing",
+      city: "Davao",
+      state: "Davao",
+      photo: "../assets/housing-1.jpg",
+      availableUnits: 4,
+      wifi: true,
+      laundry: true,
+    },
+    {
+      name: "Shai Transitional Housing",
+      city: "Dulag",
+      state: "Leyte",
+      photo: "../assets/housing-2.jpg",
+      availableUnits: 0,
+      wifi: false,
+      laundry: true,
+    },
+    {
+      name: "Nard Beds Housing Support",
+      city: "Cebu",
+      state: "Cebu",
+      photo: "../assets/housing-3.jpg",
+      availableUnits: 1,
+      wifi: false,
+      laundry: false,
+    }
+  ];
 }
